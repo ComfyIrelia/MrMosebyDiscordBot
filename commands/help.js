9c6e82d05bc7e08f1d157e3a2fba69598aac924c
Devin Harris <harrisdb@miamioh.edu>
2018-11-08 19:47:16 -0500

    Help command

--- commands/help.js
+++ commands/help.js
@@ -2,10 +2,6 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
 
+  message.channel.send("Current Commands: !8ball question, !choose option 1 | option 2 | option 3, !meme, !joke \n!multipoll unitoftime # option A | option B | option C, !nasa, !trump, !vote unitoftime # question !weather city")
 
-
-}
-
-exports.help = {
-  name:"help"
 }
